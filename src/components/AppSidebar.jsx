0be0142f4185060e5b13eef3e3c92759449f00c1
Anup Mahato <anupmahato033@gmail.com>
2022-08-13 10:34:51 +0530

    sidebar, landing page project layout

---
+++ src/components/AppSidebar.jsx
@@ -0,0 +1,57 @@
+import { Sidebar } from "flowbite-react";
+import React from "react";
+import {
+  HiAdjustments,
+  HiArrowNarrowRight,
+  HiArrowSmRight,
+  HiChartPie,
+  HiCheck,
+  HiClipboardList,
+  HiCloudDownload,
+  HiDatabase,
+  HiExclamation,
+  HiEye,
+  HiHome,
+  HiInbox,
+  HiOutlineAdjustments,
+  HiShoppingBag,
+  HiTable,
+  HiUser,
+  HiUserCircle,
+  HiViewBoards,
+  HiX,
+} from "react-icons/hi";
+
+function AppSidebar() {
+  return (
+    <Sidebar aria-label="Sidebar with multi-level dropdown example">
+      <Sidebar.Items className="bg-slate-700">
+        <Sidebar.ItemGroup className="bg-blue-300">
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Collapse icon={HiShoppingBag} label="E-commerce">
+            <Sidebar.Item href="#">Products</Sidebar.Item>
+          </Sidebar.Collapse>
+          <Sidebar.Item href="#" icon={HiInbox}>
+            Boards
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
+
+export default AppSidebar;
