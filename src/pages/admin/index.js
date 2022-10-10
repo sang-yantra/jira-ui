51042a49d01b9393444c4853a3cc486aaf5b440a
Anup Mahato <anupmahato033@gmail.com>
2022-10-10 18:48:35 +0530

    added custom table

---
+++ src/pages/admin/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Admin() {
+  return (
+    <section className="m-auto h-full w-[80%] rounded-md bg-zinc-800 p-3 text-white">
+      ADmin
+    </section>
+  );
+}
+
+export default Admin;
