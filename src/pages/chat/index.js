1e0c9896b58c8c1aef40b5eac910dc140bd6e911
Anup Mahato <anupmahato033@gmail.com>
2022-10-23 01:08:51 +0530

    chat room first implementation

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,11 +1,96 @@
 import React, { useEffect, useRef, useState } from "react";
 import Pusher from "pusher-js";
 import { CHATS_MANAGEMENT_ACTIONS } from "../../constants/api";
+import { useSelector, useDispatch } from "react-redux";
+import { setIsOpened, getSidebarOpened } from "@/store/reducers/sideBarReducer";
+import AppSidebar from "../../components/AppSidebar/AppSidebar";
+import Image from "next/image";
+
 function Chat() {
-  const channelId = "5d6e437b-a493-4042-973a-85248b018050";
+  const open = useSelector(getSidebarOpened);
+  const dispatchSideBar = useDispatch();
+  if (open) {
+    dispatchSideBar(setIsOpened());
+  }
+  const queryparams =
+    "?UserId=0c898fba-7086-449d-9d5d-463a4025a61e&Email=anupmahato033%40gmail.com";
+  const [rooms, setRooms] = useState([]);
+  const [roomId, setRoomId] = useState();
+
+  useEffect(() => {
+    fetch(CHATS_MANAGEMENT_ACTIONS.ROOMS + queryparams)
+      .then((res) => res.json())
+      .then((res) => {
+        setRooms(res);
+        if (res.length > 0) {
+          setRoomId(res[0].id);
+        }
+      });
+  }, []);
+
+  return (
+    <div className="flex h-screen w-screen justify-start bg-fuchsia-200 text-black">
+      <AppSidebar />
+      <section className="flex h-full flex-grow">
+        <div className=" flex flex-[0.2] flex-col border-r-2 shadow-xl shadow-fuchsia-500/40 ">
+          <h2 className="mt-2 mb-2 text-center font-extrabold text-fuchsia-900 ">
+            ROOMS
+          </h2>
+          <ul>
+            {rooms &&
+              rooms.map((room) => {
+                const roomTitleTrim =
+                  room.title.length > 10
+                    ? room.title.substring(0, 30) + "..."
+                    : room.title;
+                return (
+                  <li
+                    key={room.id}
+                    className="mb-2 w-full rounded-lg bg-fuchsia-300 shadow-md shadow-fuchsia-400/40
+                     hover:cursor-pointer"
+                    onClick={() => {
+                      console.log(room.id);
+                      setRoomId(room.id);
+                    }}
+                  >
+                    <div className="flex items-center gap-2 p-2">
+                      <Image
+                        width={50}
+                        height={50}
+                        className="rounded-full"
+                        src={room.users[0].avatar}
+                        alt="avatar"
+                      />
+                      <div>
+                        <h6 className=" flex-grow text-[12px] font-medium">
+                          {new Date(room.updatedDatetime).toLocaleString()}
+                        </h6>
+                        <h4>{roomTitleTrim}</h4>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+        {roomId && (
+          <div className=" flex-[0.8]">
+            <Messages channelId={roomId} />
+          </div>
+        )}
+      </section>
+    </div>
+  );
+}
+
+function Messages({ channelId }) {
+  console.log("messages");
+  debugger;
+  // const channelId = "5d6e437b-a493-4042-973a-85248b018050";
   const userId = "0c898fba-7086-449d-9d5d-463a4025a61e";
   const userName = "anup.mahato";
-
+  const queryparams =
+    "?UserId=0c898fba-7086-449d-9d5d-463a4025a61e&Email=anupmahato033%40gmail.com";
   const [message, setMessage] = useState("");
   const [chats, setChats] = useState();
   const chatref = useRef();
@@ -16,8 +101,8 @@ function Chat() {
         CHATS_MANAGEMENT_ACTIONS.GET_CHATS(channelId)
       );
       const chats = await fetchChats.json();
-      setChats(chats);
-      chatref.current = chats;
+      setChats([...chats]);
+      chatref.current = [...chats];
     }
 
     getChats();
@@ -38,7 +123,7 @@ function Chat() {
     return () => {
       pusher.disconnect();
     };
-  }, []);
+  }, [channelId]);
 
   function handleSubmit() {
     const body = {
@@ -66,7 +151,7 @@ function Chat() {
       });
   }
   return (
-    <section className="m-auto h-full w-[80%] overflow-y-auto rounded-md bg-zinc-800 p-3 text-white">
+    <section className="m-auto h-full w-full overflow-y-auto rounded-md p-3 text-black">
       <ul className="chats flex flex-col gap-2">
         {chats &&
           chats.map((chat) => {
@@ -82,7 +167,7 @@ function Chat() {
                 <div className="user_avatar h-[50px] w-[50px] rounded-full bg-white text-center text-black">
                   {chat.userName.substring(0, 2)}
                 </div>
-                <div className="user_message rounded-lg bg-gray-700 p-2">
+                <div className="user_message rounded-lg bg-fuchsia-100 p-2">
                   <h3>
                     {chat.userName}&nbsp;&nbsp;&nbsp;
                     {new Date(chat.createdDatetime).toLocaleTimeString()}{" "}
